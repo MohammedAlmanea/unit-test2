c7998d28805539f6ed7ed2a6e22678b53af56cdf
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-04 12:05:08 +0300

    Created two test specs for square function == SUCCESS

--- build/tests/indexSpec.js
+++ build/tests/indexSpec.js
@@ -6,6 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // Import all numbers.ts exports
 const numbers_1 = __importDefault(require("../utilities/numbers"));
 // Test spec for sum function in numbers.ts
-it('expect sum(5,10) to equal 15', () => {
+it('expect numbers.sum(5,10) to equal 15', () => {
     expect(numbers_1.default.sum(5, 10)).toEqual(15);
+});
+// Test spec for square function in numbers.ts
+it('expect numbers.square(10) to equal (100)', () => {
+    expect(numbers_1.default.square(10)).toEqual(100);
+});
+// Test spec for square function in numbers.ts
+it('expect numbers.square(10) to equal (100)', () => {
+    expect(numbers_1.default.square(-10)).toEqual(100);
 });
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -2,6 +2,15 @@
 import numbers from '../utilities/numbers';
 
 // Test spec for sum function in numbers.ts
-it('expect sum(5,10) to equal 15', () => {
+it('expect numbers.sum(5,10) to equal 15', () => {
   expect(numbers.sum(5,10)).toEqual(15);
+});
+
+// Test spec for square function in numbers.ts
+it('expect numbers.square(10) to equal (100)', () => {
+    expect(numbers.square(10)).toEqual(100);
+});
+// Test spec for square function in numbers.ts
+it('expect numbers.square(10) to equal (100)', () => {
+    expect(numbers.square(-10)).toEqual(100);
 });
